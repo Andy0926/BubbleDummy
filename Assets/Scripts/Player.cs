e6d0147b3b048dfd2831b5ee411e164981a3516b
Andy <tanandy2011@hotmail.com>
2020-07-12 22:46:47 +0800

    firstcommit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    [SerializeField] float runSpeed = 1.5f;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        //if the player is moving horizontally
+        bool velocity = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if(velocity)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x),1f);
+            // reverse the current scaling of x axis
+        }
+    }
+}
