a5a2788cc2e9b497422f92d59d024c147b599c85
Andy <tanandy2011@hotmail.com>
2020-07-15 17:59:21 +0800

    Added Enemy Movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidBody2D;
+    Collider2D myCollider2d;
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        myCollider2d = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFacingRight())
+        {
+            myRigidBody2D.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            myRigidBody2D.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody2D.velocity.x)),1f);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Build;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    Collider2D myCollider2d;
+    //CapsuleCollider2D capsuleCollider2D;
+    //BoxCollider2D boxCollider2D;
     [SerializeField] float runSpeed = 1.5f;
+    [SerializeField] float jumpSpeed = 10.0f;
+    [SerializeField] private Transform[] groundPoints;
+    [SerializeField ]private float groundRadius;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private LayerMask platformLayerMask;
 
+    private bool isGrounded;
     void Start()
     {
-        myRigidBody = GetComponent<Rigidbody2D>();
+        myRigidBody = transform.GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        //capsuleCollider2D = transform.GetComponent<CapsuleCollider2D>();
+        //boxCollider2D = transform.GetComponent<BoxCollider2D>();
+        myCollider2d = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Run();
+        //isGrounded = IsGrounded();
+        Jump();
+        Walk();
         FlipSprite();
+        
+        //HandleLayers();
     }
 
-    private void Run()
+    private void Walk()
     {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
@@ -42,4 +61,24 @@ public class Player : MonoBehaviour
             // reverse the current scaling of x axis
         }
     }
+    private void Jump()
+    {
+        if (!myCollider2d.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            myAnimator.SetLayerWeight(1, 0);
+            myAnimator.SetTrigger("Iddling");
+
+            Debug.Log("Touching Layer");
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(5f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+            myAnimator.SetLayerWeight(1, 1);
+            myAnimator.SetTrigger("Jumping");
+        }
+    }
+  
 }
